6e077628f4e699561a460df3431e68a75300bd7e
unknown <gksrikanth11@gmail.com>
2024-03-01 00:28:59 +0530

    EDA and problem statement

--- src/exception.py
+++ src/exception.py
@@ -8,6 +8,7 @@ def error_message_detail(error, error_detail:sys):
     error_message="Error occured in Python script name [{0}] line number [{1}] error message[{2}]".format(
         file_name, exc_tb.tb_lineno, str(error)
     )
+    return error_message
 
 
 class CustomException(Exception):
